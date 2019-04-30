b31ff1aaa7b869fff9e58619cb55f56c0c7e38d7
Pengyu Cheng <pcheng11@illinois.edu>
2019-04-29 22:36:09 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,9 +32,9 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 app.use(express.static(path.join(__dirname, "client", "build")))
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 // Use routes as a module
 require('./routes/')(app);
 // Connect to mongodb
